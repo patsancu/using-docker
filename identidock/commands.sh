2f969a9cef53f3731098735ecf7fa1c11e976b30
Patrick <patricksanzo@gmail.com>
2017-11-11 13:43:32 +0100

    add command to run with env variable
    finished identidock part - chapter 5

--- identidock/commands.sh
+++ identidock/commands.sh
@@ -1,3 +1,5 @@
 docker run -d -p 5000:5000 -v "$(pwd)"/app:/app identidock
 
 docker build -t identidock . && docker run -p 5000:9090 -p 5001:9191 identidock && docker logs -f $(docker-ps identidock)
+
+docker build -t identidock . && docker run -d -e "ENV=DEV" -p 5000:5000 identidock && docker logs -f $(docker-ps identidock)
